f797b47e2c925975b8534429246a662dba159cdb
Aleksey Petrunnik <you@example.com>
2019-08-10 12:28:57 +0400

    Из основной ветки прилоежения удалены наработки по go_downloader

--- downloaders/__init__.py
+++ downloaders/__init__.py
@@ -1,9 +1,7 @@
 from .async_downloader import AsyncDownloader
 from .thread_downloader import ThreadDownloader
-# from .go_downloader import GoDownloader
 
 downloader = {
     'async_downloader': AsyncDownloader,
     'thread_downloader': ThreadDownloader,
-    # 'go_downloader': GoDownloader,
 }
